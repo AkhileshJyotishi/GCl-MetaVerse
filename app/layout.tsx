587266627b1d004f714b8574e1fd02f9a06f70f8
AkhileshJyotishi <akhileshjyotishi1729@gmail.com>
2024-10-01 00:57:01 +0530

    autoprefixer added for cross browser compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://gcl-meta-verse.vercel.app/'),
   title: "TechM GCL 2024 Metaverse",
   description: "Tech Mahindra Global Chess League (TechM GCL) is the world’s first and largest official franchise chess league bringing fans together to witness the game in a never-seen-before avatar, a metaverse experience.",
   icons: "/favicon.ico",
